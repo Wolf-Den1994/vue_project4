459766302ead714c39e6cf8846aa5f8d3fed8da1
Denis <wolf-den@tut.by>
2021-12-29 22:55:30 +0300

    error

--- src/AppNews.vue
+++ src/AppNews.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <h3>{{ title }}</h3>
-    <button class="btn" @click="!isOpen">Открыть</button>
+    <button class="btn" @click="changeOpen">Открыть</button>
     <p v-if="isOpen">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, alias aliquid cum dolorem, dolores dolorum et exercitationem facere inventore minus molestiae porro quo rem sapiente similique sint tempora tempore voluptates?</p>
   </div>
 </template>
@@ -13,7 +13,12 @@ export default {
       item: 'item string'
     }
   },
-  // props: ['title']
+  methods: {
+    changeOpen (e) {
+      this.isOpen = !this.isOpen
+      // ESLint: Unexpected mutation of "isOpen" prop.(vue/no-mutating-props)
+    }
+  },
   props: {
     title: {
       type: String,
