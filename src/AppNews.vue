7ef4824b5d4dafee554eb87feb95e2be9ae3c5b0
Denis <wolf-den@tut.by>
2021-12-29 22:42:50 +0300

    validate props

--- src/AppNews.vue
+++ src/AppNews.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <h3>{{ title }}</h3>
-    <button class="btn" @click="isOpen = !isOpen">Открыть</button>
+    <button class="btn" @click="!isOpen">Открыть</button>
     <p v-if="isOpen">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, alias aliquid cum dolorem, dolores dolorum et exercitationem facere inventore minus molestiae porro quo rem sapiente similique sint tempora tempore voluptates?</p>
   </div>
 </template>
@@ -10,10 +10,28 @@
 export default {
   data () {
     return {
-      item: 'item string',
-      isOpen: false
+      item: 'item string'
     }
   },
-  props: ['title']
+  // props: ['title']
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: Number,
+      required: true
+    },
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: false,
+      validator (value) {
+        console.log(value)
+        return value === true || value === false
+      }
+    }
+  }
 }
 </script>
