e4e30d187e94aec8adb7304e14192dbe4f278393
Denis <wolf-den@tut.by>
2021-12-29 22:22:47 +0300

    create isolation component

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,13 @@
     <div class="card">
       <h2>Актуальные новости {{ now }}</h2>
     </div>
-    <div class="card" v-for="item in news" :key="item">
-      <h3>{{ item }}</h3>
-      <button class="btn" @click="isOpen = !isOpen">Открыть</button>
-      <p v-if="isOpen">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, alias aliquid cum dolorem, dolores dolorum et exercitationem facere inventore minus molestiae porro quo rem sapiente similique sint tempora tempore voluptates?</p>
-    </div>
+    <app-news></app-news>
+    <app-news></app-news>
   </div>
 </template>
 
 <script>
+import AppNews from './AppNews'
 export default {
   name: 'App',
   data () {
@@ -25,7 +23,9 @@ export default {
     }
   },
   components: {
-
+    // 'app-news': AppNews
+    // AppNews: AppNews
+    AppNews
   }
 }
 </script>
---
+++ src/AppNews.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <h3>{{ item }}</h3>
+    <button class="btn" @click="isOpen = !isOpen">Открыть</button>
+    <p v-if="isOpen">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, alias aliquid cum dolorem, dolores dolorum et exercitationem facere inventore minus molestiae porro quo rem sapiente similique sint tempora tempore voluptates?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      item: 'item string',
+      isOpen: false
+    }
+  }
+}
+</script>
