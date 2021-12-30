7433b49b3203482ce4137d56c2492c51eade8552
Denis <wolf-den@tut.by>
2021-12-30 23:01:28 +0300

    keep-alive

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
   <div class="container pt-1">
     <div class="card">
       <h2>Динамические и асинхронные компоненты</h2>
-      <app-button :color="active === 'one' ? 'primary' : ''" @action="active = 'one'">One</app-button>
-      <app-button :color="active === 'two' ? 'primary' : ''" @action="active = 'two'">Two</app-button>
+      <app-button :color="oneColor" @action="active = 'one'">One</app-button>
+      <app-button :color="twoColor" @action="active = 'two'">Two</app-button>
     </div>
 
 <!--    <app-text-one v-if="active === 'one'"></app-text-one>-->
 <!--    <app-text-two v-else-if="active === 'two'"></app-text-two>-->
 
-    <component :is="componentName"></component>
+    <keep-alive>
+      <component :is="componentName"></component>
+    </keep-alive>
   </div>
 </template>
 
@@ -35,6 +37,12 @@ export default {
       // }
       // return 'app-text-two'
       return 'app-text-' + this.active
+    },
+    oneColor () {
+      return this.active === 'one' ? 'primary' : ''
+    },
+    twoColor () {
+      return this.active === 'two' ? 'primary' : ''
     }
   }
 }
