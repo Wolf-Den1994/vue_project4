4c5e1e4dfd1d54763ae0bbf9b52dc1bd7c8ddc7a
Denis <wolf-den@tut.by>
2021-12-30 23:12:21 +0300

    async component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { createApp, defineAsyncComponent } from 'vue'
 import App from './App.vue'
 import TheHeader from './TheHeader'
 import './theme.css'
@@ -8,4 +8,7 @@ import './theme.css'
 const app = createApp(App)
 // global
 app.component('the-header', TheHeader)
+app.component('async-component', defineAsyncComponent(() => {
+  return import('./AppAsyncComponent')
+}))
 app.mount('#app')
